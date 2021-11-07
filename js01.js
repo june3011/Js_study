d6a75efd0d0db3a566da37d8170d6128f7ef9429
username <81845513+june3011@users.noreply.github.com>
2021-11-07 22:06:20 +0900

    Js 1
    2021-11-07

---
+++ js01.js
@@ -0,0 +1,28 @@
+alert("Hello");
+alert("World");
+// function f() {
+//     // 함수 선언문 끝엔 세미콜론이 필요 없습니다.
+//   }
+
+//   for(;;) {
+//     // 반복문 끝엔 세미콜론이 필요 없습니다.
+//   }
+let x = 5;
+console.log(x);
+x = "John";
+console.log(x);
+
+// let sum = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(sum(4, 6));
+// 10이 출력됨
+
+function addd(a, b) {
+  let result = a + b;
+
+  return result;
+}
+
+console.log(addd(60, 80));
