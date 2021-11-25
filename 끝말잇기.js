d5a2b43bba6b6cb91e1b94e21305eba9eca8980a
username <81845513+june3011@users.noreply.github.com>
2021-11-25 22:22:05 +0900

    끝말잇기
    2021-11-25

--- 끝말잇기.js
+++ 끝말잇기.js
@@ -1,27 +1,45 @@
-// chrome 창 콘솔에 치면 끝말잇기를 할 수 있습니다.
-// chrome 창 콘솔에 치면 끝말잇기를 할 수 있습니다.
+var 바디 = document.body;
+var 단어 = document.createElement("div");
+단어.textContent = "기준";
+document.body.append(단어);
+var 폼 = document.createElement("form");
+document.body.append(폼);
+var 입력창 = document.createElement("input");
+폼.append(입력창);
+var 버튼 = document.createElement("button");
+버튼.textContent = "입력!";
+폼.append(버튼);
+var 결과창 = document.createElement("div");
+document.body.append(결과창);
 
-// var word = "사사사과과";
+폼.addEventListener("submit", function 콜백함수(이벤트) {
+  이벤트.preventDefault();
+  if (단어.textContent[단어.textContent.length - 1] === 입력창.value[0]) {
+    결과창.textContent = "딩동댕";
+    단어.textContent = 입력창.value;
+    입력창.value = "";
+    입력창.focus();
+  } else {
+    결과창.textContent = "땡";
+    입력창.value = "";
+    입력창.focus();
+  }
+});
+
+// 함수 연습
+// function 더하기(숫자1, 숫자2) {
+//   console.log(숫자1 + 숫자2);
+// }
+// 더하기(3, 5);
+
+// var word = "끝말잇기";
+
 // while (true) {
 //   var answer = prompt(word);
 //   if (word[word.length - 1] === answer[0]) {
-//     //  맞았을 때
 //     alert("딩동댕");
 //     word = answer;
 //   } else {
 //     alert("땡");
 //   }
 // }
-
-for (var word = "사사사과과"; true; ) {
-  var answer = prompt(word);
-  if (word[word.length - 1] === answer[0]) {
-    //  맞았을 때
-    alert("딩동댕");
-    word = answer;
-  } else {
-    //  틀렸을 때
-    alert("땡");
-    // 틀렸다고 알려주고 다시 입력
-  }
-}
